55729d1084e65319712cabbbdc9a7524c33780c2
Michael Coates <miccoa04@student.wintec.ac.nz>
2022-06-14 14:14:56 +1200

    started the dropdown menu

--- app/src/main/java/com/example/winteckarakia/MainActivity.kt
+++ app/src/main/java/com/example/winteckarakia/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.core.os.postDelayed
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -27,9 +28,45 @@ public class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_translate -> {
 
+                true
+            }
+            R.id.action_opening_one ->{
 
+                true
+            }
+            R.id.action_opening_two -> {
 
+                true
+            }
+            R.id.action_blessing -> {
 
+                true
+            }
+            R.id.action_closing_one -> {
 
+                true
+            }
+            R.id.action_closing_two -> {
+
+                true
+            }
+            R.id.action_about_us -> {
+
+                true
+            }
+            R.id.action_history -> {
+
+                true
+            }
+            R.id.action_close -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
